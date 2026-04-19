0ceb0d1d940bc5de27b43c1ad3691ade3d844452
Anike-09 <aniketharugade009@gmail.com>
2026-04-19 23:29:40 +0530

    pusj

--- orderprocessing/OrderProcessing/src/main/java/com/orderprocessing/OrderProcessingApplication.java
+++ orderprocessing/OrderProcessing/src/main/java/com/orderprocessing/OrderProcessingApplication.java
@@ -8,9 +8,15 @@ public class OrderProcessingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OrderProcessingApplication.class, args);
+<<<<<<< HEAD
 		System.out.println("This is Main class");
 
 	
+=======
+		System.out.println("Run....");
+		System.out.println("Resolved OSM 1 Issue ");
+		System.out.println("Resolved OSM 2 Issue ");
+>>>>>>> 4a76474 (resolved OSM2)
 	}
 
 }
